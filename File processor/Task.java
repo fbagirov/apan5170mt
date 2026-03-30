e9b796249728aae0aa9ccf161e36993ee0dc6722
Minqi Huang <mh4650@columbia.edu>
2026-03-30 19:58:49 -0400

    Add synchronization locks to Task class

--- File processor/Task.java
+++ File processor/Task.java
@@ -1,6 +1,9 @@
 class Task implements Runnable {
     private int id;
 
+    private static Object lock1 = new Object();
+    private static Object lock2 = new Object();
+
     public Task(int id) {
         this.id = id;
     }
@@ -8,11 +11,18 @@ class Task implements Runnable {
     @Override
     public void run() {
         System.out.println("Task " + id + " started by " + Thread.currentThread().getName());
+
+        synchronized (lock1){
+            System.out.println("Task" + id + "lock1");
         try {
             Thread.sleep(1000); // simulate work
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        synchronized (lock2){
+            System.out.println("Task" + id + "lock2");
+        }
+    }
         System.out.println("Task " + id + " completed");
     }
 }
